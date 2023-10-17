fef2047138683a013b4268cddd4e8fe71ef8eda4
Chandni Dalal <chandnidalal2@gmail.com>
2023-10-17 14:55:23 +0530

    update a header component and make a private route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+
 const Header = () => {
+  const{currentUser} = useSelector(state => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -13,7 +16,7 @@ const Header = () => {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-
+        
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
@@ -36,12 +39,18 @@ const Header = () => {
               About
             </li>
           </Link>
-
-          <Link to="/sign-in">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+          <Link to="/profile">
+          {
+          currentUser? (
+              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avator} alt="profile"/>
+          ): (
+      <li className="hidden sm:inline text-slate-700 hover:underline">
               Sign In
             </li>
-          </Link>
+          )
+        }
+      </Link>
+          
         </ul>
       </div>
     </header>
