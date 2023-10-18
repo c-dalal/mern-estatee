d2fc56c33d75662536d332850a6078b3f15b3fa7
Chandni Dalal <chandnidalal2@gmail.com>
2023-10-18 11:51:42 +0530

    create user update API

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
 dotenv.config();
@@ -17,6 +18,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log("Server is up and running");
 });
