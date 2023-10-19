894e9e91933b549f4579694b25aed09e3b38bb26
Chandni Dalal <chandnidalal2@gmail.com>
2023-10-19 16:49:53 +0530

    creating Listing

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
+import listingRoute from "./routes/listing.route.js";
 dotenv.config();
 
 mongoose
@@ -25,6 +26,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/listing", listingRoute);
 //Creating a middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
